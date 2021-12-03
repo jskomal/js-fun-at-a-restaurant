8d4cc471413305e0d6a3d6617675104daddca10b
Jordan Skomal <jskomal@gmail.com>
2021-12-02 19:11:17 -0700

    complete order.js

--- src/order.js
+++ src/order.js
@@ -1,7 +1,60 @@
+function takeOrder(orderNum, deliveryOrders) {
+  if( deliveryOrders.length <=2) {  
+  deliveryOrders.push(orderNum);
+  }
+}
+
+function refundOrder(orderNum, deliveryOrders) { 
+  for (i=0 ; i < deliveryOrders.length ; i++) {
+    if (deliveryOrders[i].orderNumber === orderNum) {
+      deliveryOrders.splice(i, 1);
+    }
+  }
+}
+
+function listItems(array) {
+  var foodList = [];
+  for ( i = 0 ; i < array.length ; i++) {
+    foodList.push(array[i].item);
+  }
+  return foodList.join(', ')
+}
+
+function searchOrder(ordersArray, searchTerm) {
+  var foodContainer = [];
+  for (i=0 ; i < ordersArray.length; i++) {
+    foodContainer.push(ordersArray[i].item);
+  }
+  if (foodContainer.includes(searchTerm)) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+var order1 = {
+        orderNumber: 1234,
+        item: "burger",
+        price: "8.99",
+        orderType: "delivery",
+        status: "accepted"
+      };
+
+      var order2 = {
+        orderNumber: 2342,
+        item: "blt sandwich",
+        price: "5.99",
+        orderType: "delivery",
+        status: "accepted"
+      };
+
+      var deliveryOrders = [order1, order2];
+console.log(searchOrder(deliveryOrders, "burger"))
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
