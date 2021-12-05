4f4a245014ecc9b7bd7c25c1b23e06fcedb8b275
Jordan Skomal <jskomal@gmail.com>
2021-12-04 20:05:45 -0700

    complete Chef.js

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,25 @@
 class Chef {
-  constructor() {
-    
+  constructor(chefName, restaurant) {
+    this.name = chefName;
+    this.restaurant = restaurant;
+  }
+  greetCustomer(customerName, isMorning) {
+    if (isMorning === true) {
+      return `Good morning, ${customerName}!`;
+    } else {
+      return `Hello, ${customerName}!`;
+    }
+  }
+  checkForFood(foodObj) {
+    if (
+      this.restaurant.menus.breakfast.includes(foodObj) ||
+      this.restaurant.menus.lunch.includes(foodObj) ||
+      this.restaurant.menus.dinner.includes(foodObj)
+    ) {
+      return `Yes, we're serving ${foodObj.name} today!`;
+    } else {
+      return `Sorry, we aren\'t serving ${foodObj.name} today.`;
+    }
   }
 }
-
 module.exports = Chef;
